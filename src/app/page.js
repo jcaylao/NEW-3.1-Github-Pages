1f79b3516cf6c58e7329660d126a17070e2e2967
jcaylao <122569462+jcaylao@users.noreply.github.com>
2025-04-28 15:11:34 -0700

    added about page and home page

--- src/app/page.js
+++ src/app/page.js
@@ -1,10 +1,12 @@
-import Image from "next/image";
+import Link from 'next/link';
 
 export default function Home() {
-  return (
-      <div>
-        <h1>Welcome to My Portfolio!</h1>
-        <p>This is an empty React app.</p>
-      </div>
-  );
+    return (
+        <div>
+            <h1>Home Page</h1>
+            <Link href="/about">
+                Go to About
+            </Link>
+        </div>
+    );
 }
