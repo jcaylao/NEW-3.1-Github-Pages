d81e4cdfd6f4dc53b0acc7d57f0077899757d18e
jcaylao <122569462+jcaylao@users.noreply.github.com>
2025-05-05 18:57:25 -0700

    updated projects page

--- src/data/projects.js
+++ src/data/projects.js
@@ -1,15 +1,24 @@
 // /data/projects.js
 export const projects = [
     {
-        slug: "katore",
-        title: "KATORE Music Project",
-        description: "A conceptual K-pop girl group with full discography and branding.",
-        image: "/images/katore.png",
+        slug: "grocery-cooking-app",
+        title: "Grocery & Cooking App",
+        description: "A grocery and cooking app that allows users to easily find new recipes and keep track of their groceries. " +
+            "One feature is recipe tinder, in which users are given a recipe and they can choose to like or dislike a recipe. " +
+            "Another feature is a receipt scanner that allows users to scan their receipts and upload it to their virtual pantry.",
+        image: "/public/GROCERY-APP.png",
     },
     {
         slug: "synergyai",
-        title: "SynergyAI",
-        description: "An all-in-one API management dashboard for developers.",
-        image: "/images/synergyai.png",
+        title: "Redesign of API Management System",
+        description: "Redesigning the API management system WSO2, simplifying the workflow for developers. " +
+            "An all-in-one API management dashboard for developers.",
+        image: "/public/Login Page.png",
+    },
+    {
+        slug: "casestudy-synergyai",
+        title: "Case study of Redesign of API Management System",
+        description: "Website for a group case study regarding the redesign of the API management system.",
+        image: "/public/CaseStudy.png",
     },
 ];
